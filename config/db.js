276228374915c5ff8fe5004ec4b80d1aef2e9abc
LeXteRpro <lexter@hotmail.ca>
2016-03-09 04:36:46 -0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,8 @@
+// Create public connection string 
+module.exports = {
+	
+	// Local
+	// mongoose.connect('mongodb://localhost/test');
+
+	'url': 'mongodb://orie:logical@ds064748.mlab.com:64748/comp2106'
+};
